3e27be523dae87d512828da93c75b0bdb9ab16be
auasblick <auasblick@github.com>
2023-02-23 21:26:26 +0100

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include <stdio.h>
 
-int main(){
-	// test comment
-	std::cout << "Test\n";
-	
-	return 0;
+__global__
+void saxpy(int n, float a, float* x, float* y)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) y[i] = a * x[i] + y[i];
 }
+
+int main(void)
+{
+    int N = 1 << 20;
+    float* x, * y, * d_x, * d_y;
+    x = (float*)malloc(N * sizeof(float));
+    y = (float*)malloc(N * sizeof(float));
+
+    cudaMalloc(&d_x, N * sizeof(float));
+    cudaMalloc(&d_y, N * sizeof(float));
+
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    cudaMemcpy(d_x, x, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, N * sizeof(float), cudaMemcpyHostToDevice);
