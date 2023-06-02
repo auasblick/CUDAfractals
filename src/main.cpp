8a0a48ec701b128cda0ed230d8cc2613f81e6100
auasblick <auasblick@github.com>
2023-06-02 02:22:31 +0200

    Completed array summation example

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,36 @@
-#include <stdio.h>
 
-__global__
-void saxpy(int n, float a, float* x, float* y)
+// CudaTestRun.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "cuda_kernel.cuh"
+
+int main()
 {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) y[i] = a * x[i] + y[i];
+    // Initialize arrays A, B, and C.
+    double A[3], B[3], C[3];
+
+    // Populate arrays A and B.
+    A[0] = 5; A[1] = 8; A[2] = 3;
+    B[0] = 7; B[1] = 6; B[2] = 4;
+
+    // Sum array elements across ( C[0] = A[0] + B[0] ) into array C using CUDA.
+    kernel(A, B, C, 3);
+
+    // Print out result.
+    std::cout << "C = " << C[0] << ", " << C[1] << ", " << C[2] << std::endl;
+    std::cin.get();
+
+    return 0;
 }
 
-int main(void)
-{
-    int N = 1 << 20;
-    float* x, * y, * d_x, * d_y;
-    x = (float*)malloc(N * sizeof(float));
-    y = (float*)malloc(N * sizeof(float));
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
-    cudaMalloc(&d_x, N * sizeof(float));
-    cudaMalloc(&d_y, N * sizeof(float));
-
-    for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-    }
-
-    cudaMemcpy(d_x, x, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, N * sizeof(float), cudaMemcpyHostToDevice);
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
